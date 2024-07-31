3bb1d1fac6cdfd7832a7ec1c114065f352f9077b
Claudio Salvatore Arcidiacono <22871978+ClaudioSalvatoreArcidiacono@users.noreply.github.com>
2024-07-31 11:44:20 +0200

    Add docstring to callbacks, add callbacs to api reference

--- felimination/callbacks.py
+++ felimination/callbacks.py
@@ -1,5 +1,14 @@
+"""Callbacks for feature selection algorithms."""
+
+
 def plot_progress_callback(selector, *args, **kwargs):
-    """Plot the feature selection progress."""
+    """Plot the feature selection progress during the algorithm execution.
+
+    Parameters
+    ----------
+    selector : object
+        The feature selector object.
+    """
     from IPython import display
     from matplotlib import pyplot as plt
 
